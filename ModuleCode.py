5df92e4bee5e83652891d2ae306e28145ddec23d
spiiralol <78531339+spiiralol@users.noreply.github.com>
2021-05-02 18:13:08 +0100

    Update ModuleCode.py

--- ModuleCode.py
+++ ModuleCode.py
@@ -1,34 +1,34 @@
-  import cv2
-  import mediapipe as mp
-  import time
-  import HandTrackModule as htm
-  
-  ##########################
-  wCam, hCam = 1280, 720
-  ##########################
+import cv2
+import mediapipe as mp
+import time
+import HandTrackModule as htm
 
-  cap = cv2.VideoCapture(0)
-  cap.set(3, wCam)
-  cap.set(4, hCam)
-  detector = htm.handDetector()
-  pTime = 0
-  cTime = 0
+##########################
+wCam, hCam = 1280, 720
+##########################
 
-  while True:
-      success, img = cap.read()
-      img = detector.findHands(img)
-      lmList = detector.findPosition(img)
-      if len(lmList) != 0:
-          print(lmList[4])
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = htm.handDetector()
+pTime = 0
+cTime = 0
 
-      imgFlip = cv2.flip(img, 1)
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img)
+    if len(lmList) != 0:
+        print(lmList[4])
 
-      cTime = time.time()
-      fps = 1/(cTime-pTime)
-      pTime = cTime
+    imgFlip = cv2.flip(img, 1)
 
-      cv2.putText(imgFlip, f'FPS: {str(int(fps))}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
-      cv2.imshow("HandTrack AR", imgFlip)
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
 
-      if cv2.waitKey(33) == ord('q'):
-          cv2.destroyWindows()
+    cv2.putText(imgFlip, f'FPS: {str(int(fps))}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
+    cv2.imshow("HandTrack AR", imgFlip)
+
+    if cv2.waitKey(33) == ord('q'):
+        cv2.destroyWindows()
